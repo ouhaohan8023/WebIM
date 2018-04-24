1fd34721aa445dcfcd32607383ec9ad3c01ab638
Oscar <mrmercury@foxmail.com>
2018-04-24 18:51:20 +0800

    'add'

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # WebIM
 基于Swoole-WebSocket的多人在线聊天
+
+本机运行环境如下：
+* PHP 7.1.7
+* Redis 3.1.0
+* Swoole 2.1.3
+* 集成环境为LNMP1.4.1
+* 支持WebSocket的浏览器
+
+使用方法
+* 确保PHP拓展Redis，Swoole在php.ini中全部开启
+* git clone 项目到本地
+* 进入项目根目录`WebIm`，终端运行`php Socket.php`，保持窗口打开，不要关闭
+* 打开谷歌浏览器，找到项目，例如：
+`http://localhost/WebIm/Socket.html`
+* 当项目第二行出现`hello,welcome`，即表示链接成功；如果没有出现，表示链接服务器失败
+* 同理，可以查看运行中的终端
---
+++ Socket.php
@@ -0,0 +1,76 @@
+<?php
+
+//创建websocket服务器对象，监听0.0.0.0:9502端口
+$ws = new swoole_websocket_server('0.0.0.0', 9502);
+$ws->redis = new Redis();
+
+/*
+ * 监听WebSocket连接打开事件
+ */
+$ws->on('open', function ($ws, $request) {
+    $ws->redis->connect('127.0.0.1');
+    if (1 == $request->fd) {
+        $ws->redis->set('str', '');
+    }
+    $str = $ws->redis->get('str');
+    if (empty($str)) {
+        $ws->redis->set('str', $request->fd.';');
+    } else {
+        $ws->redis->set('str', $str.$request->fd.';');
+    }
+    $ws->push($request->fd, "hello, welcome\n");
+    echo '用户'.$request->fd.",进入\n";
+});
+
+/*
+ * 监听WebSocket消息事件
+ */
+$ws->on('message', function ($ws, $frame) {
+    $str = $ws->redis->get('str');
+    $Arr = explodeStr($str);
+    $msg = '【用户'.$frame->fd."】说:{$frame->data}\n";
+    foreach ($Arr as $v) {
+        echo '【用户'.$frame->fd.'】广播给【用户'.$v.'】:'.$msg;
+        $ws->push(intval($v), $msg);
+    }
+});
+
+/*
+ * 监听WebSocket连接关闭事件
+ * 删除已断开的客户端
+ */
+$ws->on('close', function ($ws, $fd) {
+    $str = $ws->redis->get('str');
+    $Arr = explodeStr($str);
+    if (empty($Arr)) {
+        echo '用户'.$fd."退出\n无用户\n";
+    } else {
+        $string = '';
+        foreach ($Arr as $k => $v) {
+            if ($v == $fd) {
+                unset($Arr[$k]);
+            } else {
+                $string = $string.$v.';';
+                $ws->redis->set('str', $string);
+            }
+        }
+        echo '用户'.$fd."退出\n";
+    }
+});
+
+$ws->start();
+
+/**
+ * 将字符串处理成数组.
+ *
+ * @param $str
+ *
+ * @return array
+ */
+function explodeStr($str)
+{
+    $str = rtrim($str, ';');
+    $Arr = explode(';', $str);
+
+    return $Arr;
+}
